587ac4d96f600173f72391901bf507aaa43a65aa
shu <shuxianshengio@126.com>
2026-05-29 14:30:59 +0800

    fake_chassis_server中注释错误问题

--- tools/fake_chassis_server.py
+++ tools/fake_chassis_server.py
@@ -1,19 +1,9 @@
 '''
  @文件路径         : /shu/projects/demo-qemu-cpp/tools/fake_chassis_server.py
  @作者           : 树
- @创建时间         : 2026-05-29 09:08:07
- @最后编辑         : 树
- @最后编辑时间       : 2026-05-29 09:08:08
- @Version      : V1.0.0
- @功能描述         :
- @Copyright    : Copyright (c) 2026 by 树, All Rights Reserved.
-'''
-'''
- @文件路径         : /shu/projects/demo-qemu/tools/fake_chassis_server.py
- @作者           : 树
  @创建时间         : 2026-05-20 15:13:55
  @最后编辑         : 树
- @最后编辑时间       : 2026-05-24 14:39:13
+ @最后编辑时间       : 2026-05-29 14:30:10
  @Version      : V1.0.0
  @功能描述         : 模拟底盘 TCP 控制服务器，用于接收控制命令并返回状态信息。
  @Copyright    : Copyright (c) 2026 by 树, All Rights Reserved.

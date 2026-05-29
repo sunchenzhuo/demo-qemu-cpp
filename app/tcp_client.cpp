29f9702950c0580dee456fec850dbdabccd6727e
shu <shuxianshengio@126.com>
2026-05-29 14:18:44 +0800

    failed拼写错误

--- app/tcp_client.cpp
+++ app/tcp_client.cpp
@@ -3,7 +3,7 @@
  * @作者           : 树
  * @创建时间         : 2026-05-27 17:31:14
  * @最后编辑         : 树
- * @最后编辑时间       : 2026-05-29 08:43:01
+ * @最后编辑时间       : 2026-05-29 14:18:21
  * @Version      : V1.0.0
  * @功能描述         :这个 TcpClient 类是对 Linux TCP socket 的一个简单封装。内部通过 sock_ 保存 socket 文件描述符，初始化为 -1 表示未连接。connectTo() 负责创建 socket、配置服务器地址并发起连接；sendText() 负责通过 send() 发送字符串数据；receiveText() 通过 recv() 接收服务器返回内容；closeSocket() 负责关闭连接并重置状态。析构函数中调用 closeSocket()，是为了保证对象销毁时自动释放 socket 资源，避免文件描述符泄漏。同时禁用拷贝构造和赋值操作，是为了防止多个对象持有同一个 socket 文件描述符，导致重复关闭或资源状态混乱。
  * @Copyright    : Copyright (c) 2026 by 树, All Rights Reserved.
@@ -63,7 +63,7 @@ bool TcpClient::connectTo(const std::string &ip, int port)
     // connect 需要 sockaddr* 类型，所以这里使用 reinterpret_cast 强制转换
     if (connect(sock_, reinterpret_cast<sockaddr *>(&server_addr), sizeof(server_addr)) < 0)
     {
-        std::cerr << "connect faild:" << ip << ":" << port << std::endl;
+        std::cerr << "connect failed:" << ip << ":" << port << std::endl;
         // 连接失败，关闭 socket
         closeSocket();
         return false;
